22d79c3de35bc5d0a09d3ce14f85e71f5bf3f914
Mark Garrison <garrison.gaming@gmail.com>
2024-06-05 15:03:55 -0400

    my resume, enjoy!

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,503 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p class="c6"><span class="c9">MARK GARRISON</span></p>
+    <p class="c12"><span class="c7">Centennial, CO | 330-780-7930 | mark.f.garrison@gmail.com | </span><span
+        class="c7 c13"><a class="c15"
+          href="http://linkedin.com/in/markgarrison">linkedin.com/in/markgarrison</a></span><span
+        class="c5">&nbsp;| Clearable</span></p>
+    <p class="c10 c12"><span class="c0"></span></p>
+    <p class="c4"><span class="c0"></span></p>
+    <p class="c6"><span class="c5">Hands-On Engineering Leader</span></p>
+    <p class="c2"><span class="c1">Accomplished technology leader with experience in overseeing and managing software
+        development and engineering functions, initiatives and personnel of an organization guide and mentor
+        application development and innovation. Background as lead developer spearheading initiatives and teams to
+        build reusable environments and tools for quickly getting new developers up to speed on the catalog of
+        technologies, designs and architectures utilized by the organization. Adept at architecting, designing,
+        building, deploying and monitoring software applications using many different platforms and technologies.
+        Experience with full micro-service lifecycle via containerization (Azure Service Fabric, Docker) and
+        orchestration (Azure Service Fabric, Kubernetes). Skilled at building agile software development teams and
+        proven track record of hiring talented and collaborative team members while guiding developers and operators
+        on creating highly performing software and deployment pipelines. Embraced the DevOps movement and strived for
+        a collaborative culture, automated self-service development environments, CI/CD pipelines, performance
+        monitoring and alerting, and measuring business value. Core Competencies include:</span></p>
+    <p class="c2 c10"><span class="c1"></span></p>
+    <p class="c2"><span class="c7">Technologies:</span><span class="c1">&nbsp;Ansible | ASP.NET | C# | GitHub | Java |
+        Kubernetes | Linux | Microsoft Azure | Microsoft SQL Server | .NET | Node.js | Puppet | Python | RavenDB | SQL
+        | Terraform | Ubuntu | Visual Studio | XML | Powershell | Bash</span></p>
+    <p class="c2"><span class="c7">Teamwork:</span><span class="c1">&nbsp;Agile Methodologies | Collaborative Problem
+        Solving | Communication | Scrum | SDLC | Servant Leadership | Shape Up | Software Project Management | DORA
+        Metrics</span></p>
+    <p class="c2"><span class="c7">Areas of Expertise:</span><span class="c1">&nbsp;Build Pipelines | Continuous
+        Integration and Continuous Delivery (CI/CD) | Developer Experience (DevEx) | DevOps | Disaster Recovery |
+        Embedded Systems | Infrastructure as Code (IaC) | Integration | Platform Engineering | Reliability Engineering
+        | Software Design | Software Engineering | Testing</span></p>
+    <p class="c4"><span class="c5"></span></p>
+    <p class="c6"><span class="c5">PROFESSIONAL EXPERIENCE</span></p>
+    <p class="c2 c10"><span class="c1"></span></p>
+    <p class="c2"><span class="c5">PartsSource Inc., Aurora, OH&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;06/2008
+        &ndash; 05/2024</span></p>
+    <p class="c2"><span class="c1">The largest online marketplace in U.S. healthcare, transforming one of the largest
+        industries in the world.</span></p>
+    <p class="c2"><span class="c7">Director of Agile Operations (Hands-On DevOps Lead)</span></p>
+    <p class="c2"><span class="c1">Architected, developed and monitored the infrastructure for https://partssource.com
+        a highly available, resilient, and performant e-commerce site utilized by 12,000 users per day to find and
+        purchase medical replacement parts and services.</span></p>
+    <ul class="c11 lst-kix_list_2-0 start">
+      <li class="c2 c8 li-bullet-0"><span class="c1">Created chatops bots using Microsoft Copilot natural language
+          processing to provide support, test, and development personnel frictionless help with their daily
+          work.</span></li>
+      <li class="c2 c8 li-bullet-0"><span class="c1">Established application performance monitoring for end-to-end
+          tracing of app and database performance. Alerted to anomalies and poor performance, providing actionable
+          insight to development teams.</span></li>
+      <li class="c2 c8 li-bullet-0"><span class="c1">Led infrastructure automation projects, achieving zero downtime
+          deployment for https://www.partssource.com and reducing deployment downtime from an average of 30 minutes
+          (100% improvement in downtime).</span></li>
+      <li class="c2 c8 li-bullet-0"><span class="c1">Set up SSL offloading and load balancing with an A or A+ SSL Labs
+          security rating for all external facing websites.</span></li>
+    </ul>
+    <p class="c2 c10"><span class="c1"></span></p>
+    <p class="c2"><span class="c5">Honeywell (Novar), Independence,
+        OH&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;09/2006 &ndash; 06/2008</span></p>
+    <p class="c2"><span class="c7">Senior Software Engineer</span></p>
+    <p class="c2"><span class="c1">Designed and developed HVAC/R control system software. Coded mostly in Java and
+        utilized the NiagaraAX framework.</span></p>
+    <ul class="c11 lst-kix_list_2-0">
+      <li class="c2 c8 li-bullet-0"><span class="c1">Mentored engineers in best practices, promoting a culture of
+          high-quality work and standards.</span></li>
+    </ul>
+    <p class="c2 c10"><span class="c1"></span></p>
+    <p class="c6"><span class="c5">ADDITIONAL PROFESSIONAL EXPERIENCE</span></p>
+    <p class="c2 c10"><span class="c1"></span></p>
+    <ul class="c11 lst-kix_list_2-0">
+      <li class="c2 c8 li-bullet-0"><span class="c5">ProQuest (formerly Bell &amp; Howell),</span><span
+          class="c1">&nbsp;Senior Software Engineer</span></li>
+      <li class="c2 c8 li-bullet-0"><span class="c5">Marconi Medical Systems (formerly Picker
+          International),</span><span class="c1">&nbsp;Senior Software Engineer</span></li>
+      <li class="c2 c8 li-bullet-0"><span class="c5">Lockheed Martin (formerly Martin Marietta),</span><span
+          class="c1">&nbsp;Systems Engineer, Secret Clear</span><span class="c3">a</span><span class="c1">nce</span>
+      </li>
+    </ul>
+    <p class="c4"><span class="c5"></span></p>
+    <p class="c6"><span class="c5">EDUCATION</span></p>
+    <p class="c2 c10"><span class="c1"></span></p>
+    <ul class="c11 lst-kix_list_2-0">
+      <li class="c2 c8 li-bullet-0"><span class="c5">Master of Science (MS), Electrical Engineering, </span><span
+          class="c1">Syracuse University, Syracuse, NY</span></li>
+      <li class="c2 c8 li-bullet-0"><span class="c5">Bachelor of Science (BS), Electrical Engineering</span><span
+          class="c1">, Northwestern University, Evanston, IL</span></li>
+    </ul>
+    <a href="https://www.linkedin.com/in/markgarrison/" target="_blank">Click here to visit Mark's LinkedIn profile</a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  name: 'MarkGarrisonProfile',
+};
+</script>
+
+<style type="text/css">
+@import url(https://themes.googleusercontent.com/fonts/css?kit=fpjTOVmNbO4Lz34iLyptLU5sOsqza4scPUlExctaWh-jUS0hn0umzC0XmKC5aH3C);
+
+.lst-kix_list_2-6>li:before {
+  content: "\0025cf   "
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.lst-kix_list_2-7>li:before {
+  content: "o  "
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+ul.lst-kix_list_1-0 {
+  list-style-type: none
+}
+
+.lst-kix_list_2-4>li:before {
+  content: "o  "
+}
+
+.lst-kix_list_2-5>li:before {
+  content: "\0025aa   "
+}
+
+.lst-kix_list_2-8>li:before {
+  content: "\0025aa   "
+}
+
+ul.lst-kix_list_2-8 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_1-3 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-2 {
+  list-style-type: none
+}
+
+.lst-kix_list_1-0>li:before {
+  content: "\0025cf   "
+}
+
+ul.lst-kix_list_1-4 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-3 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_1-1 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-0 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_1-2 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-1 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_1-7 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-6 {
+  list-style-type: none
+}
+
+.lst-kix_list_1-1>li:before {
+  content: "o  "
+}
+
+.lst-kix_list_1-2>li:before {
+  content: "\0025aa   "
+}
+
+ul.lst-kix_list_1-8 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-7 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_1-5 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-4 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_1-6 {
+  list-style-type: none
+}
+
+ul.lst-kix_list_2-5 {
+  list-style-type: none
+}
+
+.lst-kix_list_1-3>li:before {
+  content: "\0025cf   "
+}
+
+.lst-kix_list_1-4>li:before {
+  content: "o  "
+}
+
+.lst-kix_list_1-7>li:before {
+  content: "o  "
+}
+
+.lst-kix_list_1-5>li:before {
+  content: "\0025aa   "
+}
+
+.lst-kix_list_1-6>li:before {
+  content: "\0025cf   "
+}
+
+li.li-bullet-0:before {
+  margin-left: -18pt;
+  white-space: nowrap;
+  display: inline-block;
+  min-width: 18pt
+}
+
+.lst-kix_list_2-0>li:before {
+  content: "\0025cf   "
+}
+
+.lst-kix_list_2-1>li:before {
+  content: "o  "
+}
+
+.lst-kix_list_1-8>li:before {
+  content: "\0025aa   "
+}
+
+.lst-kix_list_2-2>li:before {
+  content: "\0025aa   "
+}
+
+.lst-kix_list_2-3>li:before {
+  content: "\0025cf   "
+}
+
+ol {
+  margin: 0;
+  padding: 0
+}
+
+table td,
+table th {
+  padding: 0
+}
+
+.c12 {
+  padding-top: 0pt;
+  border-bottom-color: #000000;
+  border-bottom-width: 1.5pt;
+  padding-bottom: 1pt;
+  line-height: 1.0;
+  border-bottom-style: solid;
+  orphans: 2;
+  widows: 2;
+  text-align: center
+}
+
+.c0 {
+  color: #000000;
+  font-weight: 700;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 6pt;
+  font-family: "Calibri";
+  font-style: normal
+}
+
+.c4 {
+  padding-top: 0pt;
+  padding-bottom: 0pt;
+  line-height: 1.0;
+  orphans: 2;
+  widows: 2;
+  text-align: center;
+  height: 12pt
+}
+
+.c9 {
+  color: #000000;
+  font-weight: 700;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 14pt;
+  font-family: "Calibri";
+  font-style: normal
+}
+
+.c5 {
+  color: #000000;
+  font-weight: 700;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 10.5pt;
+  font-family: "Calibri";
+  font-style: normal
+}
+
+.c1 {
+  color: #000000;
+  font-weight: 400;
+  text-decoration: none;
+  vertical-align: baseline;
+  font-size: 10.5pt;
+  font-family: "Calibri";
+  font-style: normal
+}
+
+.c2 {
+  padding-top: 0pt;
+  padding-bottom: 0pt;
+  line-height: 1.0;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+.c6 {
+  padding-top: 0pt;
+  padding-bottom: 0pt;
+  line-height: 1.0;
+  orphans: 2;
+  widows: 2;
+  text-align: center
+}
+
+.c13 {
+  -webkit-text-decoration-skip: none;
+  color: #1155cc;
+  text-decoration: underline;
+  text-decoration-skip-ink: none
+}
+
+.c7 {
+  font-size: 10.5pt;
+  font-family: "Calibri";
+  font-weight: 700
+}
+
+.c14 {
+  background-color: #ffffff;
+  max-width: 540pt;
+  padding: 36pt 36pt 36pt 36pt
+}
+
+.c3 {
+  font-size: 10.5pt;
+  font-family: "Calibri";
+  font-weight: 400
+}
+
+.c15 {
+  color: inherit;
+  text-decoration: inherit
+}
+
+.c11 {
+  padding: 0;
+  margin: 0
+}
+
+.c8 {
+  margin-left: 36pt;
+  padding-left: 0pt
+}
+
+.c10 {
+  height: 12pt
+}
+
+.title {
+  padding-top: 0pt;
+  color: #000000;
+  font-size: 28pt;
+  padding-bottom: 4pt;
+  font-family: "Play";
+  line-height: 1.0;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+.subtitle {
+  padding-top: 0pt;
+  color: #595959;
+  font-size: 14pt;
+  padding-bottom: 8pt;
+  font-family: "Aptos";
+  line-height: 1.0;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+li {
+  color: #000000;
+  font-size: 12pt;
+  font-family: "Aptos"
+}
+
+p {
+  margin: 0;
+  color: #000000;
+  font-size: 12pt;
+  font-family: "Aptos"
+}
+
+h1 {
+  padding-top: 18pt;
+  color: #0f4761;
+  font-size: 20pt;
+  padding-bottom: 4pt;
+  font-family: "Play";
+  line-height: 1.0;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+h2 {
+  padding-top: 8pt;
+  color: #0f4761;
+  font-size: 16pt;
+  padding-bottom: 4pt;
+  font-family: "Play";
+  line-height: 1.0;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+h3 {
+  padding-top: 8pt;
+  color: #0f4761;
+  font-size: 14pt;
+  padding-bottom: 4pt;
+  font-family: "Aptos";
+  line-height: 1.0;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+h4 {
+  padding-top: 4pt;
+  color: #0f4761;
+  font-size: 12pt;
+  padding-bottom: 2pt;
+  font-family: "Aptos";
+  line-height: 1.0;
+  page-break-after: avoid;
+  font-style: italic;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+h5 {
+  padding-top: 4pt;
+  color: #0f4761;
+  font-size: 12pt;
+  padding-bottom: 2pt;
+  font-family: "Aptos";
+  line-height: 1.0;
+  page-break-after: avoid;
+  orphans: 2;
+  widows: 2;
+  text-align: left
+}
+
+h6 {
+  padding-top: 2pt;
+  color: #595959;
+  font-size: 12pt;
+  padding-bottom: 0pt;
+  font-family: "Aptos";
+  line-height: 1.0;
+  page-break-after: avoid;
+  font-style: italic;
+  orphans: 2;
+  widows: 2;
+  text-align: left
 }
 </style>
